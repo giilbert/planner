1e49be2b49a181e7198ae6895147483fff465689
gil <gilbertzhang1208@gmail.com>
2021-07-28 14:20:12 -0400

    time selector styles

--- src/components/TimeSelector.tsx
+++ src/components/TimeSelector.tsx
@@ -16,90 +16,85 @@ function TimeSelector({ setFieldValue }: FormikProps<any>) {
   }, [currentTime, setFieldValue]);
 
   return (
-    <div>
-      <p>
-        {currentTime.hour}:
-        {(currentTime.minutes ?? 0) < 10
-          ? '0' + currentTime.minutes
-          : currentTime.minutes}
-        {currentTime.pm ? 'PM' : 'AM'}
-      </p>
+    <div className={styles.center}>
+      <div className={styles.container}>
+        <div className={styles.inputContainer}>
+          <input
+            className={styles.input}
+            type="number"
+            maxLength={2}
+            value={currentTime.hour.toString().replace(' ', '')}
+            onChange={(e) => {
+              const value = parseInt(e.target.value);
 
-      <p>HOURS</p>
-      <input
-        type="number"
-        maxLength={2}
-        value={currentTime.hour.toString().replace(' ', '')}
-        onChange={(e) => {
-          const value = parseInt(e.target.value);
+              if (isNaN(value)) {
+                setCurrentTime({
+                  ...currentTime,
+                  hour: 0,
+                });
+                return;
+              }
 
-          if (isNaN(value)) {
+              if (value < 1) {
+                setCurrentTime({
+                  ...currentTime,
+                  hour: 1,
+                });
+                return;
+              }
+
+              setCurrentTime({
+                ...currentTime,
+                hour: value > 12 ? 12 : value,
+              });
+            }}
+          />
+        </div>
+        <p className={styles.colon}>:</p>
+        <div className={styles.inputContainer}>
+          <input
+            type="number"
+            maxLength={2}
+            value={currentTime.minutes.toString().replace(' ', '')}
+            onChange={(e) => {
+              const value = parseInt(e.target.value);
+
+              if (isNaN(value)) {
+                setCurrentTime({
+                  ...currentTime,
+                  minutes: 0,
+                });
+                return;
+              }
+
+              if (value < 0) {
+                setCurrentTime({
+                  ...currentTime,
+                  minutes: 0,
+                });
+                return;
+              }
+
+              setCurrentTime({
+                ...currentTime,
+                minutes: value > 59 ? 59 : value,
+              });
+            }}
+          />
+        </div>
+
+        <Toggle
+          value={currentTime.pm}
+          onChange={(on) => {
             setCurrentTime({
               ...currentTime,
-              hour: 0,
+              pm: on,
             });
-            return;
-          }
-
-          if (value < 1) {
-            setCurrentTime({
-              ...currentTime,
-              hour: 1,
-            });
-            return;
-          }
-
-          setCurrentTime({
-            ...currentTime,
-            hour: value > 12 ? 12 : value,
-          });
-        }}
-      />
-
-      <p>MINUTES</p>
-      <input
-        type="number"
-        maxLength={2}
-        value={currentTime.minutes.toString().replace(' ', '')}
-        onChange={(e) => {
-          const value = parseInt(e.target.value);
-
-          if (isNaN(value)) {
-            setCurrentTime({
-              ...currentTime,
-              minutes: 0,
-            });
-            return;
-          }
-
-          if (value < 0) {
-            setCurrentTime({
-              ...currentTime,
-              minutes: 0,
-            });
-            return;
-          }
-
-          setCurrentTime({
-            ...currentTime,
-            minutes: value > 59 ? 59 : value,
-          });
-        }}
-      />
-
-      <p>PM</p>
-      <Toggle
-        value={currentTime.pm}
-        onChange={(on) => {
-          console.log(on);
-          setCurrentTime({
-            ...currentTime,
-            pm: on,
-          });
-        }}
-        offText={'Turn to AM'}
-        onText={'Turn to PM'}
-      />
+          }}
+          onText={'AM'}
+          offText={'PM'}
+        />
+      </div>
     </div>
   );
 }
@@ -124,13 +119,13 @@ function Toggle({
 
   if (on)
     return (
-      <button onClick={toggle} type="button">
+      <button onClick={toggle} type="button" className={styles.apmSelector}>
         {onText}
       </button>
     );
 
   return (
-    <button onClick={toggle} type="button">
+    <button onClick={toggle} type="button" className={styles.apmSelector}>
       {offText}
     </button>
   );
